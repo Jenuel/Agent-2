7641b37475b96c4e125519829ff89fab82c2b870
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2026-02-27 12:24:00 +0800

    Moved git commit fluency to the agents folder for modularity

--- app/backend/evaluator.py
+++ app/backend/evaluator.py
@@ -1,5 +1,4 @@
 import os
-from git import Repo
 
 REPO_DIR = "repos"
 
@@ -37,28 +36,3 @@ def read_readme(path):
                 return f.read()
 
     return ""
-
-def analyze_git(path):
-
-    repo = Repo(path)
-
-    commits = list(repo.iter_commits())
-
-    commit_count = len(commits)
-
-    descriptive = 0
-
-    for commit in commits[:50]:
-
-        msg = commit.message.lower()
-
-        if len(msg.split()) > 3:
-            descriptive += 1
-
-    quality_ratio = descriptive / max(len(commits[:50]), 1)
-
-    commit_score = min(commit_count / 100, 10)
-
-    message_score = quality_ratio * 10
-
-    return round((commit_score + message_score) / 2, 2)
